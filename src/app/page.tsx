f8cd0022c1b547402c48fe469960ec7998967172
YANGJIHUN <ik5334@naver.com>
2025-04-07 16:14:59 +0900

    로그인 필요시  로그인 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
+import Link from 'next/link'
 
 export default async function Page() {
     const session = await getServerSession(authOptions)
     console.log('세선졍보 ', session);
     return (
-        <div>
+        <div className="p-4">
             {session?.user ? (
                 <p>안녕하세요, {session.user.name}님 (ID: {session.user.id})</p>
             ) : (
-                <p>로그인이 필요합니다. </p>
+                <div>
+                <p className={"mb-2"}>로그인이 필요합니다. </p>
+                <Link
+                href ="login"
+                className ="inline-block bg-blue-500 text-white px-4 px-2 rounded">
+                로그인 하러가기
+                </Link>
+                </div>
             )}
         </div>
     )
