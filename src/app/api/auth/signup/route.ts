a96105d3c3420b04c3812db4674438d763b51345
YANGJIHUN <ik5334@naver.com>
2025-04-07 12:02:32 +0900

    회원가입 로직

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,24 @@
+import {NextResponse} from 'next/server'
+import {prisma} from  '@/lib/prisma'
+import bcrypt from 'bcryptjs'
+
+export async function POST(req:Request) {
+    const {email ,nickname , password} = await req.json()
+
+    // 중복 체크
+    const existingUser = await prisma.user.findUnique({where:{email}})
+    if (existingUser) {
+        return NextResponse.json({error: '중복된 이메일입니다.'}, {status :400})
+    }
+
+    //password hash
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    //사용사 생성
+    const user = await prisma.user.create({
+        data : { email, nickname , password : hashedPassword} ,
+    })
+
+return  NextResponse.json({ message: '회원가입 성공 ' , userId: user.id})
+
+}
