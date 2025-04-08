4685aee23f1d52431b51e4df663bf5a5e90d690d
YANGJIHUN <ik5334@naver.com>
2025-04-08 12:28:55 +0900

    레이아웃 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,17 +1,18 @@
+// src/types/next-auth.d.ts
 import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
     interface Session {
         user: {
             id: string
-            email: string
             name: string
+            email: string
         }
     }
 
     interface User {
         id: string
-        email: string
         name: string
+        email: string
     }
 }
