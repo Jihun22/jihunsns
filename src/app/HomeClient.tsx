70e0dac44649609a75f31c4a700caccd4005be5c
YANGJIHUN <ik5334@naver.com>
2025-04-08 15:35:42 +0900

    홈 화면 로직

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -1,12 +1,28 @@
 // HomeClient.tsx
-'use client' // ✅ 이거 없으면 에러 납니다!
+'use client'
 
-import { signOut } from 'next-auth/react'
+import {useSession} from 'next-auth/react'
 import LogoutButton from '@/components/LogoutButton'
 import ProfileButton from "@/components/ProfileButton";
+import {useEffect, useRef} from "react";
 
-export default function HomeClient({ session }: { session: any }) {
-    return (
+export default function HomeClient() {
+    const {data : session , status,update} = useSession()
+    const refreshed = useRef(false) //무한루프 방지
+
+    useEffect(() => {
+        if (!refreshed.current) {
+            refreshed.current = true
+            update() // 한번만 실행
+        }
+    }, [status,update])
+
+if (status == 'loading') return <p> 세션 로딩중 ..</p>
+
+if (!session?.user) {
+    return <p> 로그인 필요 </p>
+}
+        return (
         <div>
             <p>{session.user.name}님 환영합니다!(ID: {session.user.id})</p>
 
