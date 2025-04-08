e11daffa32aeca837d8a61673433b1e825c936ea
YANGJIHUN <ik5334@naver.com>
2025-04-08 12:28:32 +0900

    컴포넌트로 분리

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,4 +1,4 @@
-// src/components/LogoutButton.tsx
+// LogoutButton.tsx
 'use client'
 
 import { signOut } from 'next-auth/react'
@@ -7,7 +7,7 @@ export default function LogoutButton() {
     return (
         <button
             onClick={() => signOut({ callbackUrl: '/login' })}
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4 transition"
+            className="bg-blue-500 text-white px-4 py-2 rounded"
         >
             로그아웃
         </button>
