2a91ff9c932eb3d7cbfeac51168653e5bfcf8961
YANGJIHUN <ik5334@naver.com>
2025-04-10 16:46:59 +0900

    홈 화면에 글목록 버튼 추가  기존에 있던 글 목록  컴포넌트로 옮김

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -6,8 +6,8 @@ import LogoutButton from '@/components/LogoutButton'
 import ProfileButton from "@/components/ProfileButton";
 import {useEffect, useRef, useState} from "react";
 import LoginPage from "@/app/login/page";
-import CreatePostForm from "@/components/CreatePostForm";
 import Link from "next/link";
+import WritingListButton from "@/components/WritingListButton";
 
 export default function HomeClient() {
     const {data : session , status,update} = useSession()
@@ -21,13 +21,6 @@ export default function HomeClient() {
         }
     }, [status,update])
 
-    useEffect(() => {
-        if (session?.user) {
-            fetch("/api/post")
-                .then(res => res.json())
-            .then(data => setPosts(data))
-        }
-    }, [session]);
 
 if (status == 'loading') return <p> 세션 로딩중 ..</p>
 
@@ -58,22 +51,11 @@ if (!session?.user) {
                 <ProfileButton />
             </div>
             {/* 글 목록 */}
-            <div className="mt-6 space-y-4">
-                {posts.length === 0? (
-                    <p className="next-center text-gray-4000"> 작성된 게시글 없습니다. </p>
-                ) : (
-                posts.map((post: any) => (
-                    <div key={post.id} className="border p-4 rounded shadow-sm">
-                        <p className="text-sm text-gray-500">{post.author?.nickname}</p>
-                        <p>{post.content}</p>
-                        <p className="text-xs text-gray-400">
-                            {new Date(post.createdAt).toLocaleString()}
-                        </p>
+            <WritingListButton />
+
                     </div>
-                    ))
-                    )}
-            </div>
-        </div>
+
+
 
     )
 }
