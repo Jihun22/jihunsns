620c9919bac5a775b3cf09ec3f9c5528325aa3e4
YANGJIHUN <ik5334@naver.com>
2025-04-11 16:28:35 +0900

    토큰과 세션에 유저등급 추가

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,6 +27,7 @@ export const authOptions: NextAuthOptions = {
                     id: String(user.id),
                     email: user.email,
                     name: user.nickname,
+                    role:user.role,
                 }
             },
         }),
@@ -43,6 +44,7 @@ export const authOptions: NextAuthOptions = {
                 token.id = user.id
                 token.name = user.name
                 token.email = user.email
+                token.role = user.role
             }
             return token
         },
@@ -51,6 +53,7 @@ export const authOptions: NextAuthOptions = {
                 session.user.id = token.id as string
                 session.user.name = token.name as string
                 session.user.email = token.email as string
+                session.user.role = token.role as string
             }
             return session
         },
