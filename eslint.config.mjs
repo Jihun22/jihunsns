471147fc5142e2c8840ae2c1fe4d3150faf8d6e6
YangJihun <ik5334@naver.com>
2025-08-05 18:07:17 +0900

    feat: 규칙 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
-
-export default eslintConfig;
+export default [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@typescript-eslint/ban-ts-comment": [
+                "warn", // ✅ 오류 대신 경고로 완화
+                {
+                    tsIgnore: false,       // ✅ @ts-ignore 허용
+                    tsExpectError: true,
+                    tsNocheck: true,
+                    tsCheck: true,
+                },
+            ],
+        },
+    },
+];
